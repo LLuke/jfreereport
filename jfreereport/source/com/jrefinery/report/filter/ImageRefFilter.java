13efa54d2696644b5e129d01b394961d73fb4e90
mungady <mungady>
2002-07-16 16:41:23 +0000

    Header and Javadocs

--- jfreereport/source/com/jrefinery/report/filter/ImageRefFilter.java
+++ jfreereport/source/com/jrefinery/report/filter/ImageRefFilter.java
@@ -1,30 +1,78 @@
 /**
+ * =============================================================
+ * JFreeReport : an open source reporting class library for Java
+ * =============================================================
  *
- *  Date: 28.06.2002
- *  ImageRefFilter.java
- *  ------------------------------
- *  28.06.2002 : ...
+ * Project Info:  http://www.object-refinery.com/jfreereport;
+ * Project Lead:  Thomas Morgner (taquera@sherito.org);
+ *
+ * (C) Copyright 2000-2002, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -------------------
+ * ImageRefFilter.java
+ * -------------------
+ * (C)opyright 2002, by Thomas Morgner and Contributors.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   David Gilbert (for Simba Management Limited);
+ *
+ * $Id: First.java,v 1.2 2002/07/16 13:29:10 mungady Exp $
+ *
+ * Changes
+ * -------
+ * 28-Jun-2002 : Version 1 (TM);
+ * 16-Jul-2002 : Added standard header and Javadoc comments (DG);
+ *
  */
+
 package com.jrefinery.report.filter;
 
 import com.jrefinery.report.ImageReference;
-
 import java.awt.Image;
 import java.io.IOException;
 
+/**
+ * A filter that converts an Image to an ImageReference.
+ */
 public class ImageRefFilter implements DataFilter
 {
+  /**
+   * Default constructor.
+   */
   public ImageRefFilter ()
   {
   }
 
+  /** The data source. */
   private DataSource dataSource;
 
+  /**
+   * Returns the data source for the filter.
+   *
+   * @return The data source.
+   */
   public DataSource getDataSource ()
   {
     return dataSource;
   }
 
+  /**
+   * Sets the data source for the filter.
+   *
+   * @param dataSource The data source.
+   */
   public void setDataSource (DataSource dataSource)
   {
     this.dataSource = dataSource;
@@ -32,6 +80,8 @@ public class ImageRefFilter implements DataFilter
 
   /**
    * Returns the current value for the data source.
+   * <P>
+   * The returned object, unless it is null, will be an instance of ImageReference.
    *
    * @return The value.
    */
@@ -45,11 +95,23 @@ public class ImageRefFilter implements DataFilter
     return new ImageReference ((Image) o);
   }
 
+  /**
+   * Returns the data type for the filter.
+   * <P>
+   * The type is ImageReference.
+   *
+   * @return The data type.
+   */
   public Class getDataType ()
   {
     return ImageReference.class;
   }
 
+  /**
+   * Clones the filter.
+   *
+   * @return A clone of this filter.
+   */
   public Object clone () throws CloneNotSupportedException
   {
     ImageRefFilter r = (ImageRefFilter) super.clone ();
