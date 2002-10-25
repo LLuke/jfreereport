272c7dab8285c78ac3cdaffc0255d1c283ef1b7c
taqua <taqua>
2002-10-25 14:49:59 +0000

    BugFix: Grouped Mode was not working

--- jfreereport/source/com/jrefinery/report/function/PageTotalFunction.java
+++ jfreereport/source/com/jrefinery/report/function/PageTotalFunction.java
@@ -1,10 +1,33 @@
-/*
- * Created by IntelliJ IDEA.
- * User: user
- * Date: 16.10.2002
- * Time: 19:07:38
- * To change template for new class use
- * Code Style | Class Templates options (Tools | IDE Options).
+/**
+ * =============================================================
+ * JFreeReport : an open source reporting class library for Java
+ * =============================================================
+ *
+ * Project Info:  http://www.object-refinery.com/jfreereport;
+ * Project Lead:  Thomas Morgner (taquera@sherito.org);
+ *
+ * (C) Copyright 2000-2002, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------
+ * PageTotalFunction.java
+ * -----------------------
+ *
+ * ChangeLog
+ * ----------------
+ * 16-Oct-2002 : Initial version
+ * 25-Oct-2002 : BugFix, grouped pagecounting was not working
  */
 package com.jrefinery.report.function;
 
@@ -14,6 +37,11 @@ import com.jrefinery.report.event.ReportEvent;
 import com.jrefinery.report.states.ReportState;
 import com.jrefinery.report.util.Log;
 
+import java.util.Hashtable;
+
+/**
+ * This function will only work as expected in group mode if the named group has pagebreak set to true.
+ */
 public class PageTotalFunction extends PageFunction
 {
   /**
@@ -41,9 +69,11 @@ public class PageTotalFunction extends PageFunction
   }
 
   private PageStorage page;
+  private Hashtable groupPages;
 
   public PageTotalFunction()
   {
+    groupPages = new Hashtable();
   }
 
   /**
@@ -69,16 +99,33 @@ public class PageTotalFunction extends PageFunction
    */
   public void groupStarted(ReportEvent event)
   {
-    if (event.getState().isPrepareRun())
-    {
-      if (getGroup() == null) return;
+    if (getGroup() == null) return;
 
-      JFreeReport report = event.getReport();
-      ReportState state = event.getState();
-      Group group = report.getGroup(state.getCurrentGroupIndex());
-      if (getGroup().equals(group.getName()))
+    JFreeReport report = event.getReport();
+    ReportState state = event.getState();
+    Group group = report.getGroup(state.getCurrentGroupIndex());
+    if (getGroup().equals(group.getName()))
+    {
+      if (event.getState().isPrepareRun())
       {
+        // Correct the calculation:
+        // at this point it is sure, that the previous group has finished. We cannot use the
+        // group finished event, as this event is fired before the layouting is done. So the groupfooter
+        // can move to the next page (without throwing another event), and a function will not get
+        // informed of this.
+        this.setPage(getPage() - 1);
+
         this.page = new PageStorage(getStartPage());
+        groupPages.put(new Integer(event.getState().getCurrentDataItem()), this.page);
+      }
+      else
+      {
+        // restore the saved state
+        this.page = (PageStorage) groupPages.get(new Integer(event.getState().getCurrentDataItem()));
+        if (page == null)
+        {
+          throw new IllegalStateException("No page-storace for the current state: " + event.getState().getCurrentDataItem());
+        }
       }
     }
   }
@@ -94,7 +141,8 @@ public class PageTotalFunction extends PageFunction
   {
     if (event.getState().isPrepareRun())
     {
-      this.page = new PageStorage(getStartPage() - 1);
+      this.page = new PageStorage(getStartPage());
+      this.groupPages.clear();
     }
   }
 
@@ -154,17 +202,5 @@ public class PageTotalFunction extends PageFunction
   public int getStartPage()
   {
     return Integer.parseInt(getProperty("start", "1"));
-  }
-
-  /**
-   * Receives notification that the report has finished.
-   * <P>
-   * Maps the reportFinished-method to the legacy function endReport ().
-   *
-   * @param event Information about the event.
-   */
-  public void reportFinished(ReportEvent event)
-  {
-    event.getState().setProperty(JFreeReport.REPORT_PAGECOUNT_PROPERTY, getValue());
   }
 }
