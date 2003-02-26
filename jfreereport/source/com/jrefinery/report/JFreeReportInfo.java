1c2aace52d6fe2abf0c538c077c4fc653026c962
mungady <mungady>
2003-02-26 13:52:32 +0000

    Added Heiko Evermann to developer list.

--- jfreereport/source/com/jrefinery/report/JFreeReportInfo.java
+++ jfreereport/source/com/jrefinery/report/JFreeReportInfo.java
@@ -6,7 +6,7 @@
  * Project Info:  http://www.object-refinery.com/jfreereport/index.html
  * Project Lead:  Thomas Morgner (taquera@sherito.org);
  *
- * (C) Copyright 2000-2002, by Simba Management Limited and Contributors.
+ * (C) Copyright 2000-2003, by Simba Management Limited and Contributors.
  *
  * This library is free software; you can redistribute it and/or modify it under the terms
  * of the GNU Lesser General Public License as published by the Free Software Foundation;
@@ -23,12 +23,12 @@
  * --------------------
  * JFreeReportInfo.java
  * --------------------
- * (C)opyright 2000-2002, by Simba Management Limited and Contributors.
+ * (C)opyright 2000-2003, by Simba Management Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Simba Management Limited);
  * Contributor(s):   Thomas Morgner;
  *
- * $Id: JFreeReportInfo.java,v 1.10 2002/12/06 17:18:20 mungady Exp $
+ * $Id: JFreeReportInfo.java,v 1.11 2003/02/21 18:28:34 taqua Exp $
  *
  * Changes:
  * --------
@@ -36,19 +36,20 @@
  * 16-May-2002 : added my name to the developer list ;-) (JS)
  * 12-Nov-2002 : Minor changes required to use JCommon 0.7.1 (DG)
  * 06-Dec-2002 : Updated Javadocs (DG);
+ * 26-Feb-2003 : Added Heiko Evermann to developer list (DG);
  *
  */
 
 package com.jrefinery.report;
+
+import java.util.Arrays;
+import java.util.ResourceBundle;
 
 import com.jrefinery.JCommon;
 import com.jrefinery.ui.about.Contributor;
 import com.jrefinery.ui.about.Library;
 import com.jrefinery.ui.about.Licences;
 import com.jrefinery.ui.about.ProjectInfo;
-
-import java.util.Arrays;
-import java.util.ResourceBundle;
 
 /**
  * Details about the JFreeReport project.
@@ -79,7 +80,8 @@ public class JFreeReportInfo extends ProjectInfo implements Cloneable
             {
               new Contributor ("David Gilbert", "david.gilbert@object-refinery.com"),
               new Contributor ("Thomas Morgner", "-"),
-              new Contributor ("J\u00d6rg Sch\u00d6mer", "joerg.schoemer@nikocity.de")
+              new Contributor ("J\u00d6rg Sch\u00d6mer", "joerg.schoemer@nikocity.de"),
+              new Contributor ("Heiko Evermann", "-")
             }
     ));
 
@@ -89,7 +91,7 @@ public class JFreeReportInfo extends ProjectInfo implements Cloneable
               new Library (JCommon.INFO),
               new Library ("JUnit", "3.7.1", "IBM Public Licence", "http://www.junit.org/"),
               // 0.95 is also suitable ...
-              new Library ("iText", "0.94", "LGPL", "http://www.lowagie.com/iText/index.html"),
+              new Library ("iText", "0.96", "LGPL", "http://www.lowagie.com/iText/index.html"),
               new Library ("GNU JAXP", "1.0beta1", "GPL with library exception",
                       "http://www.gnu.org/software/classpathx/jaxp/"),
               new Library ("Pixie", "0.7.0", "LGPL",
@@ -103,7 +105,7 @@ public class JFreeReportInfo extends ProjectInfo implements Cloneable
    * Print the library version and information about the library.
    * <p>
    * After there seems to be confusion about which version is currently used by
-   * the user, this method will print the project information to clearify this
+   * the user, this method will print the project information to clarify this
    * issue.
    *
    * @param args ignored
