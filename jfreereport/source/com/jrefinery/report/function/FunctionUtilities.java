a1a48ce93b55859db630903cbe993186b845ae19
taqua <taqua>
2003-03-07 18:59:41 +0000

    Function support ...

---
+++ jfreereport/source/com/jrefinery/report/function/FunctionUtilities.java
@@ -0,0 +1,81 @@
+/**
+ * Date: Mar 7, 2003
+ * Time: 6:22:53 PM
+ *
+ * $Id$
+ */
+package com.jrefinery.report.function;
+
+import com.jrefinery.report.Element;
+import com.jrefinery.report.Band;
+import com.jrefinery.report.Group;
+import com.jrefinery.report.event.ReportEvent;
+
+public class FunctionUtilities
+{
+  /**
+   * Try to find the defined element in the last active root-band.
+   *
+   * @param band the band that is suspected to contain the element.
+   * @return the found element or null, if no element could be found.
+   */
+  public static Element findElement (Band band, String element)
+  {
+    if (element == null)
+    {
+      throw new NullPointerException("Element name must not be null");
+    }
+
+    Element[] elements = band.getElementArray();
+    for (int i = 0; i < elements.length; i++)
+    {
+      Element e = elements[i];
+      if (e instanceof Band)
+      {
+        return findElement((Band) e, element);
+      }
+      else if (e.getName().equals(element))
+      {
+        return e;
+      }
+    }
+    return null;
+  }
+
+  public static boolean isGroupInGroup (String groupName, ReportEvent event)
+  {
+    if (groupName == null)
+    {
+      return false;
+    }
+
+    Group group = event.getReport().getGroup(event.getState().getCurrentGroupIndex());
+    if (groupName.equals(group.getName()))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  public static boolean isDefinedPrepareRunLevel (Function f, ReportEvent event)
+  {
+    if (f == null)
+    {
+      throw new NullPointerException("Function is null");
+    }
+
+    if (event == null)
+    {
+      throw new NullPointerException("ReportEvent is null");
+    }
+
+    if (event.getState().isPrepareRun() == false)
+    {
+      return false;
+    }
+    return (event.getState().getLevel() == f.getDependencyLevel());
+  }
+}
---
+++ jfreereport/source/com/jrefinery/report/function/TotalCalculationFunction.java
@@ -0,0 +1,146 @@
+/**
+ * Date: Mar 7, 2003
+ * Time: 6:36:32 PM
+ *
+ * $Id$
+ */
+package com.jrefinery.report.function;
+
+import java.util.ArrayList;
+
+import com.jrefinery.report.event.ReportEvent;
+
+/**
+ * A report function that stores the result of a calculation for a group or the
+ * complete report. The field value, that was read when the group finished, is
+ * stored and returned when the group gets active again in a higher processing level.
+ * <p>
+ * This function can be used to calculate total values for a group, f.I. a TotalMax
+ * for the group, which returns the maximum value encountered for that field in the
+ * current group. The computed values are available to all bands of the group.
+ * <p>
+ * The function undestands two parameters, the <code>field</code> parameter is required and
+ * denotes the name of an ItemBand-field which gets summed up.
+ * <p>
+ * The parameter <code>group</code> denotes the name of a group. When this group is started,
+ * the counter gets reseted to null. This parameter is optional.
+ *
+ * @author Thomas Morgner
+ */
+public class TotalCalculationFunction extends AbstractFunction
+{
+  /** Literal text for the 'group' property. */
+  public static final String GROUP_PROPERTY = "group";
+
+  /** A list of results. */
+  private ArrayList storedResults;
+
+  /** The current index. */
+  private int currentIndex;
+
+  /** The current object. */
+  private Object currentObject;
+
+  /**
+   * Constructs a new function.
+   * <P>
+   * Initially the function has no name...be sure to assign one before using the function.
+   */
+  public TotalCalculationFunction ()
+  {
+    storedResults = new ArrayList();
+  }
+
+
+  /**
+   * Receives notification that the report has started.
+   *
+   * @param event  the event.
+   */
+  public void reportStarted(ReportEvent event)
+  {
+    currentIndex = -1;
+
+    if (FunctionUtilities.isDefinedPrepareRunLevel(this, event))
+    {
+      storedResults.clear();
+    }
+  }
+
+  /**
+   * Receives notification that a group has started.
+   *
+   * @param event  the event.
+   */
+  public void groupStarted(ReportEvent event)
+  {
+    if (FunctionUtilities.isGroupInGroup(getGroup(), event) == false)
+    {
+      // wrong group ...
+      return;
+    }
+
+    if (FunctionUtilities.isDefinedPrepareRunLevel(this, event))
+    {
+      currentObject = null;
+    }
+    else
+    {
+      // Activate the current group, which was filled in the prepare run.
+      currentIndex += 1;
+      currentObject = storedResults.get(currentIndex);
+    }
+  }
+
+  /**
+   * Receives notification that a group has finished.
+   *
+   * @param event  the event.
+   */
+  public void groupFinished(ReportEvent event)
+  {
+    if (FunctionUtilities.isGroupInGroup(getGroup(), event) == false)
+    {
+      // wrong group ...
+      return;
+    }
+
+    if (FunctionUtilities.isDefinedPrepareRunLevel(this, event))
+    {
+      storedResults.add(currentObject);
+    }
+  }
+
+  /**
+   * Returns the name of the group to be totalled.
+   *
+   * @return the group name.
+   */
+  public String getGroup()
+  {
+    return getProperty(GROUP_PROPERTY);
+  }
+
+  /**
+   * Defines the name of the group to be totalled.
+   * If the name is null, all groups are totalled.
+   *
+   * @param group  the group name.
+   */
+  public void setGroup(String group)
+  {
+    setProperty(GROUP_PROPERTY, group);
+  }
+
+  /**
+   * Return the current expression value.
+   * <P>
+   * The value depends (obviously) on the expression implementation.
+   *
+   * @return the value of the function.
+   */
+  public Object getValue()
+  {
+    return currentObject;
+  }
+}
