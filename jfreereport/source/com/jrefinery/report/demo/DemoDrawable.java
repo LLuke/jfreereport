db5f0d2f9a972fd123fc13bba91b58c38da385ea
taqua <taqua>
2003-03-09 17:19:28 +0000

    Drawable stuff ..

---
+++ jfreereport/source/com/jrefinery/report/demo/DemoDrawable.java
@@ -0,0 +1,29 @@
+/**
+ * Date: Mar 9, 2003
+ * Time: 6:19:34 PM
+ *
+ * $Id$
+ */
+package com.jrefinery.report.demo;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+import com.jrefinery.ui.Drawable;
+
+public class DemoDrawable implements Drawable
+{
+  public DemoDrawable()
+  {
+  }
+
+  public void draw(Graphics2D graphics, Rectangle2D bounds)
+  {
+    graphics.setColor(Color.black);
+    graphics.drawString(bounds.toString(), 10, 10);
+    graphics.draw(new Rectangle2D.Double(0,0,453,30));
+    graphics.draw(bounds);
+  }
+}
+
