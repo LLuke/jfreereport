e454667df19a55812d35f995f54e2dfacd0ef361
taqua <taqua>
2003-05-26 14:25:35 +0000

    Some silly tests ..

---
+++ jfreereport-ext/source/com/jrefinery/report/ext/junit/base/ArrayTest.java
@@ -0,0 +1,51 @@
+/**
+ * ========================================
+ * JFreeReport : a free Java report library
+ * ========================================
+ *
+ * Project Info:  http://www.object-refinery.com/jfreereport/index.html
+ * Project Lead:  Thomas Morgner (taquera@sherito.org);
+ *
+ * (C) Copyright 2000-2002, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * ----------------
+ * ArrayTest.java
+ * ----------------
+ * (C)opyright 2002, by Thomas Morgner and Contributors.
+ *
+ * Original Author:  Thomas Morgner (taquera@sherito.org);
+ * Contributor(s):   David Gilbert (for Simba Management Limited);
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ * 14.04.2003 : Initial version
+ */
+package com.jrefinery.report.ext.junit.base;
+
+public class ArrayTest
+{
+  public static void main (String [] a) throws Exception
+  {
+    Object o = new Object[10];
+    Object o2 = new int[10];
+
+    Class.forName(o2.getClass().getName());
+    Class.forName(o.getClass().getName());
+    System.out.println (o.getClass().getComponentType());
+    System.out.println (o2.getClass().getComponentType());
+  }
+}
---
+++ jfreereport-ext/source/com/jrefinery/report/ext/junit/bugs/POITest.java
@@ -0,0 +1,54 @@
+/**
+ * ========================================
+ * JFreeReport : a free Java report library
+ * ========================================
+ *
+ * Project Info:  http://www.object-refinery.com/jfreereport/index.html
+ * Project Lead:  Thomas Morgner (taquera@sherito.org);
+ *
+ * (C) Copyright 2000-2003, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * ------------------------------
+ * POITest.java
+ * ------------------------------
+ * (C)opyright 2003, by Thomas Morgner and Contributors.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   David Gilbert (for Simba Management Limited);
+ *
+ * $Id$
+ *
+ * Changes 
+ * -------------------------
+ * 09.05.2003 : Initial version
+ *  
+ */
+
+package com.jrefinery.report.ext.junit.bugs;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class POITest
+{
+  public static void main (String[] args)
+  {
+    HSSFWorkbook wb = new HSSFWorkbook();
+    wb.createCellStyle();
+    wb.createDataFormat();
+    wb.createFont();
+    wb.createSheet();
+
+  }
+}
