ed89a1156b327be4d259678345275112e98cbb5f
taqua <taqua>
2003-06-10 18:14:28 +0000

    DataSource reference documentation ...

---
+++ jfreereport/source/com/jrefinery/report/io/ext/factory/datasource/DataSourceReferenceGenerator.java
@@ -0,0 +1,110 @@
+/**
+ * ========================================
+ * JFreeReport : a free Java report library
+ * ========================================
+ *
+ * Project Info:  http://www.object-refinery.com/jfreereport/index.html
+ * Project Lead:  Thomas Morgner (taquera@sherito.org);
+ *
+ * (C) Copyright 2000-2003, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -------------------------------
+ * StyleKeyReferenceGenerator.java
+ * -------------------------------
+ * (C)opyright 2003, by Thomas Morgner and Contributors.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   David Gilbert (for Simba Management Limited);
+ *
+ * $Id: StyleKeyReferenceGenerator.java,v 1.4 2003/05/02 12:40:13 taqua Exp $
+ *
+ * Changes (from 19-Feb-2003)
+ * -------------------------
+ * 19-Feb-2003 : Added standard header and Javadocs (DG);
+ *  
+ */
+
+package com.jrefinery.report.io.ext.factory.datasource;
+
+import java.net.URL;
+
+import com.jrefinery.report.JFreeReport;
+import com.jrefinery.report.io.ReportGenerator;
+import com.jrefinery.report.targets.support.ReportProcessorUtil;
+
+/**
+ * An application that generates a report that provides style key reference information.
+ * 
+ * @author Thomas Morgner.
+ */
+public class DataSourceReferenceGenerator
+{
+  /** The report definition file. */    
+  private static final String REFERENCE_REPORT =
+    "/com/jrefinery/report/io/ext/factory/datasource/DataSourceReferenceReport.xml";
+
+  /**
+   * The starting point for the application.
+   * 
+   * @param args  ignored.
+   */
+  public static void main (String [] args)
+  {
+    DataSourceCollector cc = new DataSourceCollector();
+    cc.addFactory(new DefaultDataSourceFactory());
+
+    DataSourceReferenceTableModel model = new DataSourceReferenceTableModel(cc);
+
+    ReportGenerator gen = ReportGenerator.getInstance();
+    URL reportURL = gen.getClass().getResource(REFERENCE_REPORT);
+    if (reportURL == null)
+    {
+      System.err.println ("The report was not found in the classpath");
+      System.err.println ("File: " + REFERENCE_REPORT);
+      System.exit(1);
+      return;
+    }
+
+    JFreeReport report;
+    try
+    {
+      report = gen.parseReport(reportURL);
+    }
+    catch (Exception e)
+    {
+      System.err.println ("The report could not be parsed.");
+      System.err.println ("File: " + REFERENCE_REPORT);
+      e.printStackTrace(System.err);
+      System.exit(1);
+      return;
+    }
+    report.setData(model);
+    try
+    {
+      ReportProcessorUtil.createStreamHTML(report, System.getProperty("user.home") 
+                                                   + "/datasource-reference.html");
+      ReportProcessorUtil.createPDF(report, System.getProperty("user.home") 
+                                            + "/datasource-reference.pdf");
+    }
+    catch (Exception e)
+    {
+      System.err.println ("The report processing failed.");
+      System.err.println ("File: " + REFERENCE_REPORT);
+      e.printStackTrace(System.err);
+      System.exit(1);
+    }
+  }
+
+}
---
+++ jfreereport/source/com/jrefinery/report/io/ext/factory/datasource/DataSourceReferenceTableModel.java
@@ -0,0 +1,249 @@
+/**
+ * ========================================
+ * JFreeReport : a free Java report library
+ * ========================================
+ *
+ * Project Info:  http://www.object-refinery.com/jfreereport/index.html
+ * Project Lead:  Thomas Morgner (taquera@sherito.org);
+ *
+ * (C) Copyright 2000-2003, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * --------------------------------
+ * StyleKeyReferenceTableModel.java
+ * --------------------------------
+ * (C)opyright 2003, by Thomas Morgner and Contributors.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   David Gilbert (for Simba Management Limited);
+ *
+ * $Id: StyleKeyReferenceTableModel.java,v 1.4 2003/05/02 12:40:13 taqua Exp $
+ *
+ * Changes (from 19-Feb-2003)
+ * -------------------------
+ * 19-Feb-2003 : Added standard header and Javadocs (DG);
+ *  
+ */
+
+package com.jrefinery.report.io.ext.factory.datasource;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import javax.swing.table.AbstractTableModel;
+
+import org.jfree.xml.factory.objects.ObjectDescription;
+
+/**
+ * A table model for the style key reference generator.
+ * 
+ * @author Thomas Morgner
+ */
+public class DataSourceReferenceTableModel extends AbstractTableModel
+{
+  /**
+   * Represents a row in the table model.
+   */
+  private class DataSourceDescriptionRow
+  {
+    /** The factory. */
+    private DataSourceFactory datasourceFactory;
+    
+    /** The key. */
+    private String datasourceName;
+
+    /** The implementing class for the datasource name. */
+    private Class implementingClass;
+
+    /**
+     * Creates a new row.
+     *
+     * @param datasourceFactory the datasource factory
+     * @param name the name of the datasource within the factory.
+     */
+    public DataSourceDescriptionRow(DataSourceFactory datasourceFactory,
+                                  String name, Class implementingClass)
+    {
+      this.datasourceFactory = datasourceFactory;
+      this.datasourceName = name;
+      this.implementingClass = implementingClass;
+    }
+
+    /**
+     * Returns the factory.
+     * 
+     * @return The factory.
+     */
+    public DataSourceFactory getFactory()
+    {
+      return datasourceFactory;
+    }
+
+    /**
+     * Returns the key.
+     * 
+     * @return The key.
+     */
+    public String getName()
+    {
+      return datasourceName;
+    }
+
+    public Class getImplementingClass()
+    {
+      return implementingClass;
+    }
+  }
+
+  /** The column names. */
+  private static final String[] COLUMN_NAMES =
+      {
+        "datasource-factory",
+        "datasource-name",
+        "datasource-class"
+      };
+
+  /** Storage for the rows. */
+  private ArrayList rows;
+
+  /**
+   * Creates a new table model.
+   * 
+   * @param cf  the factory collection.
+   */
+  public DataSourceReferenceTableModel(DataSourceCollector cf)
+  {
+    rows = new ArrayList();
+    addFactoryCollector(cf);
+  }
+
+  /**
+   * Adds a factory.
+   * 
+   * @param cf  the factory.
+   */
+  private void addFactoryCollector (DataSourceCollector cf)
+  {
+    Iterator it = cf.getFactories();
+    while (it.hasNext())
+    {
+      DataSourceFactory cfact = (DataSourceFactory) it.next();
+      if (cfact instanceof DataSourceCollector)
+      {
+        addFactoryCollector((DataSourceCollector) cfact);
+      }
+      else
+      {
+        addDataSourceFactory(cfact);
+      }
+    }
+  }
+
+  /**
+   * Adds a factory.
+   * 
+   * @param cf  the factory.
+   */
+  private void addDataSourceFactory (DataSourceFactory cf)
+  {
+    Iterator it = cf.getRegisteredNames();
+    ArrayList factories = new ArrayList();
+
+    while (it.hasNext())
+    {
+      String c = (String) it.next();
+      factories.add(c);
+    }
+
+    Collections.sort(factories);
+    it = factories.iterator();
+
+    while (it.hasNext())
+    {
+      String keyName = (String) it.next();
+      ObjectDescription od = cf.getDataSourceDescription(keyName);
+      rows.add(new DataSourceDescriptionRow(cf, keyName, od.getObjectClass()));
+    }
+  }
+
+  /**
+   * Returns the number of rows in the model. A
+   * <code>JTable</code> uses this method to determine how many rows it
+   * should display.  This method should be quick, as it
+   * is called frequently during rendering.
+   *
+   * @return the number of rows in the model
+   * @see #getColumnCount
+   */
+  public int getRowCount()
+  {
+    return rows.size();
+  }
+
+  /**
+   * Returns the number of columns in the model. A
+   * <code>JTable</code> uses this method to determine how many columns it
+   * should create and display by default.
+   *
+   * @return the number of columns in the model
+   * @see #getRowCount
+   */
+  public int getColumnCount()
+  {
+    return COLUMN_NAMES.length;
+  }
+
+  /**
+   * Returns the column name.
+   * 
+   * @param column  the column being queried
+   * @return a string containing the default name of <code>column</code>
+   */
+  public String getColumnName(int column)
+  {
+    return COLUMN_NAMES[column];
+  }
+
+  /**
+   *  Returns <code>String.class</code> regardless of <code>columnIndex</code>.
+   *
+   *  @param columnIndex  the column being queried
+   *  @return the Object.class
+   */
+  public Class getColumnClass(int columnIndex)
+  {
+    return String.class;
+  }
+
+  /**
+   * Returns the value for the cell at <code>columnIndex</code> and
+   * <code>rowIndex</code>.
+   *
+   * @param rowIndex  the row whose value is to be queried
+   * @param columnIndex  the column whose value is to be queried
+   * 
+   * @return the value Object at the specified cell
+   */
+  public Object getValueAt(int rowIndex, int columnIndex)
+  {
+    DataSourceDescriptionRow or = (DataSourceDescriptionRow) rows.get(rowIndex);
+    switch (columnIndex)
+    {
+      case 0: return String.valueOf(or.getFactory().getClass().getName());
+      case 1: return String.valueOf(or.getName());
+      case 2: return String.valueOf(or.getImplementingClass().getName());
+    }
+    return null;
+  }
+}
