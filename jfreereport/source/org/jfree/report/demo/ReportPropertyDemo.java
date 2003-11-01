b5e4532535134efc5a92f91333896646e09e0e8b
taqua <taqua>
2003-11-01 13:45:18 +0000

    New demo added: Report Property demo

---
+++ jfreereport/source/org/jfree/report/demo/ReportPropertyDemo.java
@@ -0,0 +1,213 @@
+/**
+ * ========================================
+ * JFreeReport : a free Java report library
+ * ========================================
+ *
+ * Project Info:  http://www.jfree.org/jfreereport/index.html
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2000-2002, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -------------------
+ * PercentageDemo.java
+ * -------------------
+ * (C)opyright 2003, by Simba Management Limited.
+ *
+ * Original Author:  David Gilbert (for Simba Management Limited);
+ * Contributor(s):   -;
+ *
+ * $Id: PercentageDemo.java,v 1.3 2003/09/15 15:31:58 taqua Exp $
+ *
+ * Changes
+ * -------
+ * 04-Apr-2003 : Version 1 (DG);
+ *
+ */
+
+package org.jfree.report.demo;
+
+import java.awt.BorderLayout;
+import java.net.URL;
+import java.text.MessageFormat;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.jfree.report.Boot;
+import org.jfree.report.JFreeReport;
+import org.jfree.report.ReportProcessingException;
+import org.jfree.report.demo.helper.AbstractDemoFrame;
+import org.jfree.report.modules.gui.base.PreviewFrame;
+import org.jfree.report.modules.gui.base.components.ActionButton;
+import org.jfree.report.modules.gui.base.components.ActionMenuItem;
+import org.jfree.report.modules.parser.base.ReportGenerator;
+import org.jfree.report.util.Log;
+import org.jfree.ui.RefineryUtilities;
+
+/**
+ * A simple report that shows the user input as report property value.
+ *
+ * @author Thomas Morgner
+ */
+public class ReportPropertyDemo extends AbstractDemoFrame
+{
+  private JTextArea textArea;
+
+  /**
+   * Constructs the demo application.
+   *
+   * @param title  the frame title.
+   */
+  public ReportPropertyDemo(final String title)
+  {
+    setTitle(title);
+    setJMenuBar(createMenuBar());
+    setContentPane(createContent());
+  }
+
+  /**
+   * Creates a menu bar.
+   *
+   * @return the menu bar.
+   */
+  public JMenuBar createMenuBar()
+  {
+    final JMenuBar mb = new JMenuBar();
+    final JMenu fileMenu = createJMenuItem("menu.file");
+
+    final JMenuItem previewItem = new ActionMenuItem(getPreviewAction());
+    final JMenuItem exitItem = new ActionMenuItem(getCloseAction());
+
+    fileMenu.add(previewItem);
+    fileMenu.addSeparator();
+    fileMenu.add(exitItem);
+    mb.add(fileMenu);
+    return mb;
+  }
+
+  /**
+   * Creates the content for the application frame.
+   *
+   * @return a panel containing the basic user interface.
+   */
+  public JPanel createContent()
+  {
+    final JPanel content = new JPanel(new BorderLayout());
+    content.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+
+    final String d = "Enter your text here, it will be displayed in the report.";
+    textArea = new JTextArea(d);
+    textArea.setLineWrap(true);
+    textArea.setWrapStyleWord(true);
+    textArea.setEditable(true);
+    final JScrollPane scroll = new JScrollPane(textArea);
+    scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+    final JButton previewButton = new ActionButton(getPreviewAction());
+
+    content.add(scroll, BorderLayout.CENTER);
+    content.add(previewButton, BorderLayout.SOUTH);
+    return content;
+  }
+
+  /**
+   * Displays a print preview screen for the sample report.
+   */
+  protected void attemptPreview()
+  {
+    final URL in = getClass().getResource("/org/jfree/report/demo/ReportPropertyDemo.xml");
+
+    if (in == null)
+    {
+      JOptionPane.showMessageDialog(this,
+          MessageFormat.format(getResources().getString("report.definitionnotfound"),
+              new Object[]{in}),
+          getResources().getString("error"), JOptionPane.ERROR_MESSAGE);
+    }
+
+    final JFreeReport report;
+    try
+    {
+      report = parseReport(in);
+      report.setProperty("Message", textArea.getText());
+    }
+    catch (Exception ex)
+    {
+      showExceptionDialog("report.definitionfailure", ex);
+      return;
+    }
+
+    try
+    {
+      final PreviewFrame frame = new PreviewFrame(report);
+      frame.getBase().setToolbarFloatable(true);
+      frame.pack();
+      RefineryUtilities.positionFrameRandomly(frame);
+      frame.setVisible(true);
+      frame.requestFocus();
+    }
+    catch (ReportProcessingException pre)
+    {
+      showExceptionDialog("report.previewfailure", pre);
+    }
+  }
+
+  /**
+   * Reads the report from the specified template file.
+   *
+   * @param templateURL  the template location.
+   *
+   * @return a report.
+   */
+  private JFreeReport parseReport(final URL templateURL)
+  {
+
+    JFreeReport result = null;
+    final ReportGenerator generator = ReportGenerator.getInstance();
+    try
+    {
+      result = generator.parseReport(templateURL);
+    }
+    catch (Exception e)
+    {
+      Log.error("Failed to parse the report definition", e);
+    }
+    return result;
+
+  }
+
+  /**
+   * Entry point for running the demo application...
+   *
+   * @param args  ignored.
+   */
+  public static void main(final String[] args)
+  {
+    // initialize JFreeReport
+    Boot.start();
+
+    final ReportPropertyDemo frame = new ReportPropertyDemo("ReportProperty Demo");
+    frame.pack();
+    RefineryUtilities.centerFrameOnScreen(frame);
+    frame.setVisible(true);
+  }
+
+}
