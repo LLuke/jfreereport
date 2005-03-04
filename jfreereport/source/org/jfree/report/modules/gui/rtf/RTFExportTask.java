7971ca1222c5ffd44cb03322d52f7e6122a930f1
taqua <taqua>
2005-03-04 13:17:23 +0000

    New, but very basic RTF export GUI module

---
+++ jfreereport/source/org/jfree/report/modules/gui/rtf/RTFExportTask.java
@@ -0,0 +1,184 @@
+/**
+ * ========================================
+ * JFreeReport : a free Java report library
+ * ========================================
+ *
+ * Project Info:  http://www.jfree.org/jfreereport/index.html
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2000-2003, by Object Refinery Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * ------------------------------
+ * ExcelExportTask.java
+ * ------------------------------
+ * (C)opyright 2003, by Thomas Morgner and Contributors.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   David Gilbert (for Object Refinery Limited);
+ *
+ * $Id: ExcelExportTask.java,v 1.10 2005/02/23 21:05:03 taqua Exp $
+ *
+ * Changes
+ * -------------------------
+ * 24.08.2003 : Initial version
+ *
+ */
+
+package org.jfree.report.modules.gui.rtf;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.jfree.report.JFreeReport;
+import org.jfree.report.ReportInterruptedException;
+import org.jfree.report.modules.gui.base.ExportTask;
+import org.jfree.report.modules.gui.base.ReportProgressDialog;
+import org.jfree.report.modules.output.table.rtf.RTFProcessor;
+import org.jfree.report.util.Log;
+
+/**
+ * An export task implementation, which writes a given report into an Excel file.
+ *
+ * @author Thomas Morgner
+ */
+public class RTFExportTask extends ExportTask
+{
+  /**
+   * The progress dialog that will be used to visualize the report progress.
+   */
+  private final ReportProgressDialog progressDialog;
+  /**
+   * The file name of the output file.
+   */
+  private final String fileName;
+  /**
+   * The report which should be exported.
+   */
+  private final JFreeReport report;
+
+  /**
+   * Creates a new export task.
+   *
+   * @param fileName the name of the target file.
+   * @param dialog   the progress dialog that will monitor the report progress.
+   * @param report   the report that should be exported.
+   */
+  public RTFExportTask
+          (final String fileName, final ReportProgressDialog dialog,
+           final JFreeReport report)
+  {
+    if (fileName == null)
+    {
+      throw new NullPointerException("File name is null.");
+    }
+    if (report == null)
+    {
+      throw new NullPointerException("Report is null.");
+    }
+    this.fileName = fileName;
+    this.progressDialog = dialog;
+    this.report = report;
+  }
+
+  /**
+   * Exports the report into an Excel file.
+   */
+  protected void performExport ()
+  {
+    OutputStream out = null;
+    final File file = new File(fileName);
+    try
+    {
+      out = new BufferedOutputStream(new FileOutputStream(file));
+      final RTFProcessor target = new RTFProcessor(report);
+      if (progressDialog != null)
+      {
+        progressDialog.setModal(false);
+        progressDialog.setVisible(true);
+        target.addRepaginationListener(progressDialog);
+      }
+      target.setOutputStream(out);
+      target.processReport();
+      out.close();
+      if (progressDialog != null)
+      {
+        target.removeRepaginationListener(progressDialog);
+      }
+      setTaskDone();
+    }
+    catch (ReportInterruptedException re)
+    {
+      setTaskAborted();
+      try
+      {
+        out.close();
+        out = null;
+        if (file.delete() == false)
+        {
+          Log.warn(new Log.SimpleMessage("Unable to delete incomplete export:", file));
+        }
+      }
+      catch (SecurityException se)
+      {
+        // ignore me
+      }
+      catch (IOException ioe)
+      {
+        // ignore me...
+      }
+    }
+    catch (Exception re)
+    {
+      Log.error("RTF export failed", re);
+      setTaskFailed(re);
+    }
+    finally
+    {
+      try
+      {
+        if (out != null)
+        {
+          out.close();
+        }
+      }
+      catch (Exception e)
+      {
+        Log.error("Unable to close the output stream.", e);
+        setTaskFailed(e);
+        // if there is already another error, this exception is
+        // just a minor obstactle. Something big crashed before ...
+      }
+    }
+    if (progressDialog != null)
+    {
+      progressDialog.setVisible(false);
+    }
+  }
+
+  /**
+   * Remove all listeners and prepare the finalization.
+   */
+  protected void dispose ()
+  {
+    super.dispose();
+    if (progressDialog != null)
+    {
+      progressDialog.dispose();
+    }
+  }
+}
