cbb86f5d298c1968f57b8d1b258291cbaeb42d46
taqua <taqua>
2005-12-21 20:19:52 +0000

    Html symbolic color names fixed;

---
+++ jfreereport/source/org/jfree/report/modules/output/table/html/util/HtmlColors.java
@@ -0,0 +1,67 @@
+/**
+ * ========================================
+ * <libname> : a free Java <foobar> library
+ * ========================================
+ *
+ * Project Info:  http://www.jfree.org/liblayout/
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2005, by Object Refinery Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * ---------
+ * HtmlColors.java
+ * ---------
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributors: -;
+ *
+ * $Id: Anchor.java,v 1.3 2005/02/23 21:04:29 taqua Exp $
+ *
+ * Changes
+ * -------------------------
+ * 20.12.2005 : Initial version
+ */
+package org.jfree.report.modules.output.table.html.util;
+
+import java.awt.Color;
+
+
+/**
+ * Creation-Date: 20.12.2005, 10:48:22
+ *
+ * @author Thomas Morgner
+ */
+public class HtmlColors
+{
+  public static Color BLACK = new Color(0x000000, false);
+  public static Color GREEN = new Color(0x008000, false);
+  public static Color SILVER = new Color(0xC0C0C0, false);
+  public static Color LIME = new Color(0x00FF00, false);
+  public static Color GRAY = new Color(0x808080, false);
+  public static Color OLIVE = new Color(0x808000, false);
+  public static Color WHITE = new Color(0xFFFFFF, false);
+  public static Color YELLOW = new Color(0xFFFF00, false);
+  public static Color MAROON = new Color(0x800000, false);
+  public static Color NAVY = new Color(0x000080, false);
+  public static Color RED = new Color(0xFF0000, false);
+  public static Color BLUE = new Color(0x0000FF, false);
+  public static Color PURPLE = new Color(0x800080, false);
+  public static Color TEAL = new Color(0x008080, false);
+  public static Color FUCHSIA = new Color(0xFF00FF, false);
+  public static Color AQUA = new Color(0x00FFFF, false);
+
+  private HtmlColors() {}
+
+}
