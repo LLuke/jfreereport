467d8380d0b288e8edae9c6e158f8d499ade80da
taqua <taqua>
2005-12-22 01:22:30 +0000

    New file

---
+++ jfreereport-ext/source/org/jfree/report/ext/servletdemo/DemoConstants.java
@@ -0,0 +1,49 @@
+/**
+ * ========================================
+ * <libname> : a free Java <foobar> library
+ * ========================================
+ *
+ * Project Info:  http://www.jfree.org/liblayout/
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2005, by Object Refinery Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * ---------
+ * DemoConstants.java
+ * ---------
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributors: -;
+ *
+ * $Id: Anchor.java,v 1.3 2005/02/23 21:04:29 taqua Exp $
+ *
+ * Changes
+ * -------------------------
+ * 22.12.2005 : Initial version
+ */
+package org.jfree.report.ext.servletdemo;
+
+/**
+ * Creation-Date: 22.12.2005, 02:07:21
+ *
+ * @author Thomas Morgner
+ */
+public class DemoConstants
+{
+  public static final String REPORT_DEFINITION = "org/jfree/report/demo/swingicons/swing-icons.xml";
+
+  private DemoConstants() {
+  }
+}
