1a50d2cf761702621933b59ad03ee358dd1e3b6d
taqua <taqua>
2006-01-27 16:26:20 +0000

    New files for the state bug

---
+++ jfreereport/source/org/jfree/report/demo/layouts/ShapeAndDrawableDemoHandler.java
@@ -0,0 +1,114 @@
+/**
+ * ========================================
+ * JFreeReport : a free Java report library
+ * ========================================
+ *
+ * Project Info:  http://www.jfree.org/jfreereport/index.html
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2000-2002, by Simba Management Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -------------------
+ * PaintComponentDemoHandler.java
+ * -------------------
+ * (C)opyright 2003, by Simba Management Limited.
+ *
+ * Original Author:  David Gilbert (for Simba Management Limited);
+ * Contributor(s):   -;
+ *
+ * $Id: ComponentDrawingDemoHandler.java,v 1.3 2005/11/17 17:03:47 taqua Exp $
+ *
+ * Changes
+ * -------
+ * 04-Apr-2003 : Version 1 (DG);
+ *
+ */
+
+package org.jfree.report.demo.layouts;
+
+import java.net.URL;
+import javax.swing.JColorChooser;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+
+import org.jfree.report.JFreeReport;
+import org.jfree.report.JFreeReportBoot;
+import org.jfree.report.demo.functions.PaintComponentTableModel;
+import org.jfree.report.demo.helper.AbstractXmlDemoHandler;
+import org.jfree.report.demo.helper.ReportDefinitionException;
+import org.jfree.report.demo.helper.SimpleDemoFrame;
+import org.jfree.ui.RefineryUtilities;
+import org.jfree.util.ObjectUtilities;
+
+/**
+ * A simple report where column 3 displays (column 1 / column 2) as a percentage.
+ *
+ * @author David Gilbert
+ */
+public class ShapeAndDrawableDemoHandler extends AbstractXmlDemoHandler
+{
+
+  /**
+   * Constructs the demo application.
+   */
+  public ShapeAndDrawableDemoHandler ()
+  {
+  }
+
+  public String getDemoName()
+  {
+    return "Component Drawing Demo";
+  }
+
+  public JFreeReport createReport() throws ReportDefinitionException
+  {
+    final JFreeReport report = parseReport();
+    return report;
+  }
+
+  public URL getDemoDescriptionSource()
+  {
+    return ObjectUtilities.getResourceRelative
+            ("shape-and-drawable.html", ShapeAndDrawableDemoHandler.class);
+  }
+
+
+
+  public JComponent getPresentationComponent()
+  {
+    return new JPanel();
+  }
+
+  public URL getReportDefinitionSource()
+  {
+    return ObjectUtilities.getResourceRelative
+            ("shape-and-drawable.xml", ShapeAndDrawableDemoHandler.class);
+  }
+
+
+  public static void main (final String[] args)
+  {
+    JFreeReportBoot.getInstance().start();
+
+    final ShapeAndDrawableDemoHandler demoHandler = new ShapeAndDrawableDemoHandler();
+    final SimpleDemoFrame frame = new SimpleDemoFrame(demoHandler);
+    frame.init();
+    frame.pack();
+    RefineryUtilities.centerFrameOnScreen(frame);
+    frame.setVisible(true);
+
+  }
+}
