9d2686b1045b92cdcb560e4a5336315a829d9a3b
taqua <taqua>
2006-07-11 13:21:18 +0000

    LibXML sync complete ..

---
+++ libxml/source/org/jfree/xmlns/parser/IgnoreAnyChildReadHandler.java
@@ -0,0 +1,83 @@
+/**
+ * =========================================
+ * LibLayout : a free Java layouting library
+ * =========================================
+ *
+ * Project Info:  http://www.jfree.org/liblayout/
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2006, by Pentaho Corperation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * ------------
+ * IgnoreAnyChildReadHandler.java
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corperation.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   -;
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ *
+ *
+ */
+package org.jfree.xmlns.parser;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+
+/**
+ * Creation-Date: 03.07.2006, 14:33:08
+ *
+ * @author Thomas Morgner
+ */
+public class IgnoreAnyChildReadHandler extends AbstractXmlReadHandler
+{
+  public IgnoreAnyChildReadHandler()
+  {
+  }
+
+  /**
+   * Returns the handler for a child element.
+   *
+   * @param tagName the tag name.
+   * @param atts    the attributes.
+   * @return the handler or null, if the tagname is invalid.
+   * @throws org.xml.sax.SAXException if there is a parsing error.
+   */
+  protected XmlReadHandler getHandlerForChild(final String uri,
+                                              final String tagName,
+                                              final Attributes atts)
+          throws SAXException
+  {
+    return new IgnoreAnyChildReadHandler();
+  }
+
+  /**
+   * Returns the object for this element or null, if this element does not
+   * create an object.
+   *
+   * @return the object.
+   */
+  public Object getObject() throws SAXException
+  {
+    return null;
+  }
+}
