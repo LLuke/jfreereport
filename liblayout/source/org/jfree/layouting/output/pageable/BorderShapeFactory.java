a9c91500195c48331aef9a02a544e7c5c8eb9043
taqua <taqua>
2006-07-11 13:54:24 +0000

    New files

---
+++ liblayout/source/org/jfree/layouting/output/pageable/BorderShapeFactory.java
@@ -0,0 +1,368 @@
+/**
+ * ===========================================
+ * LibLayout : a free Java layouting library
+ * ===========================================
+ *
+ * Project Info:  http://www.jfree.org/liblayout/
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2000-2005, by Object Refinery Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * ------------
+ * BorderShapeFactory.java
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   -;
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ *
+ *
+ */
+package org.jfree.layouting.output.pageable;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+import org.jfree.layouting.renderer.border.Border;
+import org.jfree.layouting.renderer.border.BorderCorner;
+import org.jfree.layouting.renderer.border.BorderEdge;
+import org.jfree.layouting.renderer.model.RenderBox;
+import org.jfree.layouting.util.geom.StrictGeomUtility;
+import org.jfree.layouting.util.geom.StrictInsets;
+import org.jfree.layouting.input.style.values.CSSColorValue;
+
+/**
+ * Creation-Date: 11.07.2006, 13:32:31
+ *
+ * @author Thomas Morgner
+ */
+public class BorderShapeFactory
+{
+  private static final int CORNER_RIGHT_TOP = 0;
+  private static final int CORNER_TOP_RIGHT = 1;
+  private static final int CORNER_TOP_LEFT = 2;
+  private static final int CORNER_LEFT_TOP = 3;
+  private static final int CORNER_LEFT_BOTTOM = 4;
+  private static final int CORNER_BOTTOM_LEFT = 5;
+  private static final int CORNER_BOTTOM_RIGHT = 6;
+  private static final int CORNER_RIGHT_BOTTOM = 7;
+
+  private static final byte[][] CORNER_FACTORS = new byte[][] {
+          { +1, +1}, // RIGHT_TOP
+          { +1, +1}, // TOP_RIGHT
+          { -1, +1}, // TOP_LEFT
+          { -1, +1}, // LEFT_TOP
+          { +1, -1}, // LEFT_BOTTOM
+          { +1, -1}, // BOTTOM_LEFT
+          { -1, -1}, // BOTTOM_RIGHT
+          { -1, -1}, // RIGHT_BOTTOM
+  };
+
+
+  public static class BorderDrawOperation
+  {
+    private Color color;
+    private BasicStroke stroke;
+    private Shape shape;
+
+    public BorderDrawOperation(final Shape shape,
+                               final Color color,
+                               final BasicStroke stroke)
+    {
+      this.shape = shape;
+      this.color = color;
+      this.stroke = stroke;
+    }
+
+    public void draw(Graphics2D g2)
+    {
+      if (shape == null) return;
+      if (stroke.getLineWidth() == 0) return;
+      if (color.getAlpha() == 0) return;
+
+      g2.setStroke(stroke);
+      g2.setColor(color);
+      g2.draw(shape);
+    }
+
+    public void fill(final Graphics2D g2)
+    {
+      if (shape == null) return;
+      if (stroke.getLineWidth() == 0) return;
+      if (color.getAlpha() == 0) return;
+
+      g2.setStroke(stroke);
+      g2.setColor(color);
+      g2.fill(shape);
+    }
+  }
+
+  private Color color;
+  private BasicStroke stroke;
+  private double height;
+  private double width;
+  private double x;
+  private double y;
+  private Border border;
+  private StrictInsets borderSizes;
+  private ArrayList drawOps;
+  private ArrayList fillOps;
+  private CSSColorValue backgroundColor;
+
+  public BorderShapeFactory(final RenderBox box)
+  {
+    drawOps = new ArrayList();
+    fillOps = new ArrayList();
+
+    x = StrictGeomUtility.toExternalValue
+            (box.getX() + box.getMargins().getLeft());
+    y = StrictGeomUtility.toExternalValue
+            (box.getY() + box.getMargins().getTop());
+    width = StrictGeomUtility.toExternalValue
+            (box.getWidth() - box.getMargins().getLeft() -
+                    box.getMargins().getRight());
+    height = StrictGeomUtility.toExternalValue
+            (box.getHeight() - box.getMargins().getTop() -
+                    box.getMargins().getBottom());
+
+    border = box.getBorder();
+    borderSizes = box.getBorderWidths();
+    backgroundColor = box.getBoxDefinition().getBackgroundColor();
+  }
+
+  private Arc2D generateCorner (int type,
+                                double cornerX,
+                                double cornerY,
+                                BorderCorner corner,
+                                boolean fillShape)
+  {
+    if (corner.getHeight().getValue() > 0 && corner.getWidth().getValue() > 0)
+    {
+      byte[] cornerFactors = CORNER_FACTORS[type];
+
+      final double widthTopLeft =
+              StrictGeomUtility.toExternalValue(corner.getWidth().getValue());
+      final double heightTopLeft =
+              StrictGeomUtility.toExternalValue(corner.getHeight().getValue());
+
+      final int open;
+      if (fillShape)
+      {
+        open = Arc2D.PIE;
+      }
+      else
+      {
+        open = Arc2D.OPEN;
+      }
+      return new Arc2D.Double
+              (cornerX + widthTopLeft * cornerFactors[0],
+                      cornerY + heightTopLeft * cornerFactors[1],
+                      widthTopLeft, heightTopLeft,
+                      Math.PI * type / 4.0, Math.PI / 4.0, open);
+    }
+    return null;
+  }
+
+  private BasicStroke createStroke (BorderEdge edge, long width)
+  {
+    return new BasicStroke(width / 1000f);
+  }
+
+  public void generateBorder (Graphics2D g2)
+  {
+    generateTopEdge();
+    generateLeftEdge();
+    generateBottomEdge();
+    geenrateRightEdge();
+
+    for (int i = 0; i < drawOps.size(); i++)
+    {
+      BorderDrawOperation operation = (BorderDrawOperation) drawOps.get(i);
+      operation.draw(g2);
+    }
+
+    for (int i = 0; i < fillOps.size(); i++)
+    {
+      BorderDrawOperation operation = (BorderDrawOperation) fillOps.get(i);
+      operation.fill(g2);
+    }
+
+    if (backgroundColor == null || backgroundColor.getAlpha() == 0)
+    {
+      return;
+    }
+    // now we need some geometry stuff (yeah, I'm lazy!)
+    Area globalArea = new Area(new Rectangle2D.Double(x, y, width, height));
+
+    // for each corner:
+    //
+    // create the inverse area and substract that from
+    // the global area.
+    for (int i = 0; i < fillOps.size(); i++)
+    {
+      BorderDrawOperation operation = (BorderDrawOperation) fillOps.get(i);
+      final Shape shape = operation.shape;
+      if (shape == null)
+      {
+        continue;
+      }
+
+      Area cornerArea = new Area(shape.getBounds2D());
+      cornerArea.subtract(new Area (shape));
+      globalArea.subtract(cornerArea);
+    }
+    // oh, yeah, I know, there are faster ways than that.
+
+    g2.setColor(backgroundColor);
+    g2.fill(globalArea);
+  }
+
+  private void draw (Shape s)
+  {
+    drawOps.add(new BorderDrawOperation(s, color, stroke));
+  }
+
+  private void fill (Shape s)
+  {
+    fillOps.add(new BorderDrawOperation(s, color, stroke));
+  }
+
+  private void geenrateRightEdge ()
+  {
+    final BorderEdge rightEdge = border.getRight();
+    color = rightEdge.getColor();
+    stroke = createStroke(rightEdge, borderSizes.getRight());
+
+    final BorderCorner firstCorner = border.getBottomRight();
+    final BorderCorner secondCorner = border.getTopRight();
+
+    draw(generateCorner(CORNER_RIGHT_BOTTOM, x + width, y + height, firstCorner, false));
+    draw(generateCorner(CORNER_RIGHT_TOP, x + width, y, secondCorner, false));
+    fill(generateCorner(CORNER_RIGHT_BOTTOM, x + width, y + height, firstCorner, true));
+    fill(generateCorner(CORNER_RIGHT_TOP, x + width, y, secondCorner, true));
+
+    draw (new Line2D.Double
+            (x + width - firstCorner.getWidth().getValue(),
+             y + height - firstCorner.getHeight().getValue(),
+             x + width - secondCorner.getWidth().getValue(),
+             y + secondCorner.getHeight().getValue()));
+
+  }
+
+  private void generateBottomEdge ()
+  {
+    final BorderEdge bottomEdge = border.getBottom();
+    final BorderCorner firstCorner = border.getBottomLeft();
+    final BorderCorner secondCorner = border.getBottomRight();
+
+    color = bottomEdge.getColor();
+    stroke = createStroke(bottomEdge, borderSizes.getBottom());
+
+    draw(generateCorner(CORNER_BOTTOM_LEFT, x, y + height, firstCorner, false));
+    draw(generateCorner(CORNER_BOTTOM_RIGHT, x + width, y + height, secondCorner, false));
+    fill(generateCorner(CORNER_BOTTOM_LEFT, x, y + height, firstCorner, true));
+    fill(generateCorner(CORNER_BOTTOM_RIGHT, x + width, y + height, secondCorner, true));
+
+    draw (new Line2D.Double
+            (x + firstCorner.getWidth().getValue(),
+             y + height - firstCorner.getHeight().getValue(),
+             x + width - secondCorner.getWidth().getValue(),
+             y + height - secondCorner.getHeight().getValue()));
+
+  }
+
+  private void generateLeftEdge ()
+  {
+    final BorderEdge leftEdge = border.getLeft();
+
+    final BorderCorner firstCorner = border.getTopLeft();
+    final BorderCorner secondCorner = border.getBottomLeft();
+
+    stroke = createStroke(leftEdge, borderSizes.getTop());
+    color = leftEdge.getColor();
+
+    draw(generateCorner
+      (CORNER_LEFT_TOP, x, y, firstCorner, false));
+    draw(generateCorner
+      (CORNER_LEFT_BOTTOM, x, y + height, secondCorner, false));
+    fill(generateCorner
+      (CORNER_LEFT_TOP, x, y, firstCorner, true));
+    fill(generateCorner
+      (CORNER_LEFT_BOTTOM, x, y + height, secondCorner, true));
+
+    final double firstWidth =
+            StrictGeomUtility.toInternalValue(firstCorner.getWidth().getValue());
+    final double firstHeight =
+            StrictGeomUtility.toInternalValue(firstCorner.getHeight().getValue());
+    final double secondWidth =
+            StrictGeomUtility.toInternalValue(secondCorner.getWidth().getValue());
+    final double secondHeight =
+            StrictGeomUtility.toInternalValue(secondCorner.getHeight().getValue());
+    draw(new Line2D.Double
+            (x + firstWidth,
+             y + firstHeight,
+             x + secondWidth,
+             y + height - secondHeight));
+  }
+
+  private void generateTopEdge ()
+  {
+    final BorderEdge topEdge = border.getTop();
+    stroke = createStroke(topEdge, borderSizes.getTop());
+    color = topEdge.getColor();
+
+    final BorderCorner firstCorner = border.getTopLeft();
+    final BorderCorner secondCorner = border.getBottomLeft();
+
+    draw(generateCorner
+            (CORNER_TOP_RIGHT, x + width, y, border.getTopRight(), false));
+    draw(generateCorner
+            (CORNER_TOP_LEFT, x, y, border.getTopLeft(), false));
+
+    fill(generateCorner
+            (CORNER_TOP_RIGHT, x + width, y, border.getTopRight(), true));
+    fill(generateCorner
+            (CORNER_TOP_LEFT, x, y, border.getTopLeft(), true));
+
+    final double firstWidth =
+            StrictGeomUtility.toInternalValue(firstCorner.getWidth().getValue());
+    final double firstHeight =
+            StrictGeomUtility.toInternalValue(firstCorner.getHeight().getValue());
+    final double secondWidth =
+            StrictGeomUtility.toInternalValue(secondCorner.getWidth().getValue());
+    final double secondHeight =
+            StrictGeomUtility.toInternalValue(secondCorner.getHeight().getValue());
+    draw(new Line2D.Double
+            (x + firstWidth, y + firstHeight,
+             x + width - secondWidth, y + secondHeight));
+  }
+
+
+}
---
+++ liblayout/source/org/jfree/layouting/output/pageable/graphics/PageDrawable.java
@@ -0,0 +1,293 @@
+/**
+ * ===========================================
+ * LibLayout : a free Java layouting library
+ * ===========================================
+ *
+ * Project Info:  http://www.jfree.org/liblayout/
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2000-2005, by Object Refinery Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * ------------
+ * GraphicsRenderer.java
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   -;
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ *
+ *
+ */
+package org.jfree.layouting.output.pageable.graphics;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.Shape;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+import org.jfree.fonts.awt.AWTFontRegistry;
+import org.jfree.layouting.input.style.keys.color.ColorStyleKeys;
+import org.jfree.layouting.input.style.values.CSSColorValue;
+import org.jfree.layouting.layouter.context.FontSpecification;
+import org.jfree.layouting.layouter.context.LayoutContext;
+import org.jfree.layouting.renderer.border.Border;
+import org.jfree.layouting.renderer.border.BorderCorner;
+import org.jfree.layouting.renderer.border.BorderEdge;
+import org.jfree.layouting.renderer.model.RenderBox;
+import org.jfree.layouting.renderer.model.RenderNode;
+import org.jfree.layouting.renderer.model.RenderNodeState;
+import org.jfree.layouting.renderer.model.RenderableText;
+import org.jfree.layouting.renderer.model.BoxDefinition;
+import org.jfree.layouting.renderer.text.Glyph;
+import org.jfree.layouting.util.geom.StrictGeomUtility;
+import org.jfree.layouting.util.geom.StrictInsets;
+import org.jfree.layouting.output.pageable.BorderShapeFactory;
+import org.jfree.ui.Drawable;
+import org.jfree.util.Log;
+
+/**
+ * Creation-Date: 25.06.2006, 16:27:28
+ *
+ * @author Thomas Morgner
+ */
+public class PageDrawable implements Drawable
+{
+  private RenderBox rootBox;
+  private AWTFontRegistry fontRegistry;
+
+  public PageDrawable(final RenderBox rootBox)
+  {
+    this.rootBox = rootBox;
+    this.fontRegistry = new AWTFontRegistry();
+  }
+
+  /**
+   * Draws the object.
+   *
+   * @param g2   the graphics device.
+   * @param area the area inside which the object should be drawn.
+   */
+  public void draw(Graphics2D g2, Rectangle2D area)
+  {
+    g2.setPaint(Color.WHITE);
+    g2.fill(area);
+
+    g2.translate(-area.getX(), -area.getY());
+    //g2.scale(4, 4);
+    final Rectangle2D.Double bounds = new Rectangle2D.Double
+            (rootBox.getX() / 1000f, rootBox.getY() / 1000f,
+                    rootBox.getWidth() / 1000f, rootBox.getHeight() / 1000f);
+    g2.setPaint(Color.BLACK);
+    g2.draw(bounds);
+    drawBox(g2, rootBox, 0);
+  }
+
+
+
+  public void drawBox(Graphics2D g2, RenderBox box, int level)
+  {
+    if (box.getState() == RenderNodeState.UNCLEAN)
+    {
+      Log.warn("Box is unclean: " + level + ": " + box + " (" + box.getParent() + ")");
+    }
+
+    final double x = StrictGeomUtility.toExternalValue
+            (box.getX() + box.getMargins().getLeft());
+    final double y = StrictGeomUtility.toExternalValue
+            (box.getY() + box.getMargins().getTop());
+    final double w = StrictGeomUtility.toExternalValue
+            (box.getWidth() - box.getMargins().getLeft() +
+                    box.getMargins().getRight());
+    final double h = StrictGeomUtility.toExternalValue
+            (box.getHeight() - box.getMargins().getTop() +
+                    box.getMargins().getBottom());
+
+    BorderShapeFactory borderShapeFactory = new BorderShapeFactory(box);
+    borderShapeFactory.generateBorder(g2);
+
+    RenderNode childs = box.getFirstChild();
+    while (childs != null)
+    {
+      if (childs instanceof RenderBox)
+      {
+        drawBox(g2, (RenderBox) childs, level + 1);
+      }
+      else if (childs instanceof RenderableText)
+      {
+        drawText(g2, (RenderableText) childs);
+      }
+      childs = childs.getNext();
+    }
+  }
+
+  public AWTFontRegistry getFontRegistry()
+  {
+    return fontRegistry;
+  }
+
+  private void drawText(final Graphics2D g2,
+                        final RenderableText renderableText)
+  {
+    Glyph[] gs = renderableText.getGlyphs();
+    long posX = renderableText.getX();
+    long posY = renderableText.getY();
+    long runningPos = posX;
+
+    final LayoutContext layoutContext = renderableText.getLayoutContext();
+    final FontSpecification fontSpecification =
+            layoutContext.getFontSpecification();
+
+    int style = Font.PLAIN;
+    if (fontSpecification.getFontWeight() > 400)
+    {
+      style |= Font.BOLD;
+    }
+    if (fontSpecification.isItalic() || fontSpecification.isOblique())
+    {
+      style |= Font.ITALIC;
+    }
+
+    final CSSColorValue cssColor = (CSSColorValue)
+            layoutContext.getStyle().getValue(ColorStyleKeys.COLOR);
+
+    g2.setColor(cssColor);
+    g2.setFont(new Font(fontSpecification.getFontFamily(), style,
+            (int) fontSpecification.getFontSize() ));
+
+    int length = renderableText.getOffset() + renderableText.getLength();
+    for (int i = renderableText.getOffset(); i < length; i++)
+    {
+      Glyph g = gs[i];
+      g2.drawString(glpyhToString(g), runningPos / 1000f, (posY + g.getBaseLine()) / 1000f);
+      runningPos += g.getWidth();
+    }
+  }
+
+  public static String glpyhToString(Glyph g)
+  {
+    StringBuffer b = new StringBuffer();
+    b.append((char) (0xffff & g.getCodepoint()));
+    int[] extraCPs = g.getExtraChars();
+    for (int i = 0; i < extraCPs.length; i++)
+    {
+      b.append(", ");
+      int extraCP = extraCPs[i];
+      b.append(extraCP);
+    }
+    return b.toString();
+  }
+
+
+  public void print()
+  {
+    printBox(rootBox, 0);
+  }
+
+  public void printBox(RenderBox box, int level)
+  {
+    if (box.isKillMePlease())
+    {
+      throw new IllegalStateException();
+    }
+
+    StringBuffer b = new StringBuffer();
+    for (int i = 0; i < level; i++)
+    {
+      b.append("   ");
+    }
+    b.append(box.getClass().getName());
+    b.append("[");
+    b.append(Integer.toHexString(System.identityHashCode(box)));
+    b.append("]");
+    b.append("={x=");
+    b.append(box.getX());
+    b.append(", y=");
+    b.append(box.getY());
+    b.append(", width=");
+    b.append(box.getWidth());
+    b.append(", height=");
+    b.append(box.getHeight());
+    b.append(", padding=");
+    b.append(box.getPaddings());
+    b.append("}");
+    Log.debug(b.toString());
+
+    RenderNode childs = box.getFirstChild();
+    while (childs != null)
+    {
+      if (childs instanceof RenderBox)
+      {
+        printBox((RenderBox) childs, level + 1);
+      }
+      else if (childs instanceof RenderableText)
+      {
+        printText((RenderableText) childs, level + 1);
+      }
+      childs = childs.getNext();
+    }
+  }
+
+  private void printText(final RenderableText text, final int level)
+  {
+    if (text.isKillMePlease())
+    {
+      throw new IllegalStateException();
+    }
+
+    StringBuffer b = new StringBuffer();
+    for (int i = 0; i < level; i++)
+    {
+      b.append("   ");
+    }
+    b.append("Text");
+    b.append("[");
+    b.append(Integer.toHexString(System.identityHashCode(text)));
+    b.append("]");
+    b.append("={x=");
+    b.append(text.getX());
+    b.append(", y=");
+    b.append(text.getY());
+    b.append(", width=");
+    b.append(text.getWidth());
+    b.append(", height=");
+    b.append(text.getHeight());
+    b.append(", text=");
+
+    Glyph[] gs = text.getGlyphs();
+    int length = text.getOffset() + text.getLength();
+    for (int i = text.getOffset(); i < length; i++)
+    {
+      Glyph g = gs[i];
+      b.append(glpyhToString(g));
+    }
+    b.append("}");
+    Log.debug(b.toString());
+  }
+
+}
