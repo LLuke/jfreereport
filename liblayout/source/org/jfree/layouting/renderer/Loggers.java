cc7ea51ac591c4b8975f3d3c6808d36cb38f2d07
taqua <taqua>
2006-07-27 17:57:29 +0000

    general Loggers to tame the renderer's output ..

---
+++ liblayout/source/org/jfree/layouting/renderer/Loggers.java
@@ -0,0 +1,60 @@
+/**
+ * ===========================================
+ * LibLayout : a free Java layouting library
+ * ===========================================
+ *
+ * Project Info:  http://www.jfree.org/liblayout/
+ * Project Lead:  Thomas Morgner;
+ *
+ * (C) Copyright 2000-2005, by Object Refinery Limited and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * ------------
+ * Loggers.java
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ *
+ * Original Author:  Thomas Morgner;
+ * Contributor(s):   -;
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ *
+ *
+ */
+package org.jfree.layouting.renderer;
+
+import org.jfree.util.LogContext;
+import org.jfree.util.Log;
+
+/**
+ * Creation-Date: 27.07.2006, 14:35:31
+ *
+ * @author Thomas Morgner
+ */
+public class Loggers
+{
+  private Loggers ()
+  {
+  }
+  
+  public static final LogContext VALIDATION = Log.createContext("Renderer.validation");
+  public static final LogContext STATECHANGE = Log.createContext("Renderer.statechange");
+  public static final LogContext SPLITSTRATEGY = Log.createContext("Renderer.split");
+}
