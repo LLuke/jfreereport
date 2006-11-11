b79b722c3f423884ddcd4c9f3010eee42175a5e2
taqua <taqua>
2006-11-11 20:41:14 +0000

    New files

---
+++ jfreereport/source/org/jfree/report/flow/AbstractReportProcessor.java
@@ -0,0 +1,76 @@
+/**
+ * ========================================
+ * JFreeReport : a free Java report library
+ * ========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/
+ *
+ * (C) Copyright 2000-2006, by Object Refinery Limited, Pentaho Corporation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * ------------
+ * $Id$
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ */
+
+package org.jfree.report.flow;
+
+import org.jfree.report.ReportDataFactoryException;
+import org.jfree.report.DataSourceException;
+import org.jfree.report.ReportProcessingException;
+
+/**
+ * Creation-Date: 10.11.2006, 16:07:26
+ *
+ * @author Thomas Morgner
+ */
+public abstract class AbstractReportProcessor implements ReportProcessor
+{
+  public AbstractReportProcessor()
+  {
+  }
+
+  protected void processReportRun(final ReportJob job, final ReportTarget target)
+      throws ReportDataFactoryException,
+      DataSourceException, ReportProcessingException
+  {
+    synchronized (job)
+    {
+      // set up the scene
+      final LayoutControler layoutControler = new DefaultLayoutControler();
+
+      // we have the data and we have our position inside the report.
+      // lets generate something ...
+      final FlowControler flowControler = createFlowControler(job);
+      LayoutPosition position = layoutControler.createInitialPosition
+              (flowControler, job.getReport());
+      while (position.isFinalPosition() == false)
+      {
+        position = layoutControler.process(target, position);
+        target.commit();
+      }
+    }
+  }
+
+  protected FlowControler createFlowControler(ReportJob job)
+          throws DataSourceException
+  {
+    return new DefaultFlowControler(job);
+  }
+
+}
