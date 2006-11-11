ada750c8a3b2394bfd94844a4b7ca5b0cd43c283
taqua <taqua>
2006-11-11 20:13:23 +0000

    Modules

---
+++ jfreereport-ext/source/org/jfree/report/ext/modules/output/java14print/Java14PrintModule.java
@@ -0,0 +1,70 @@
+/**
+ * ===========================================
+ * JFreeReport : a free Java reporting library
+ * ===========================================
+ *
+ * Project Info:  http://www.jfree.org/liblayout/
+ *
+ * (C) Copyright 2006, by Pentaho Corperation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * ------------
+ * Java14PrintModule.java
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corperation.
+ */
+
+package org.jfree.report.ext.modules.output.java14print;
+
+import org.jfree.base.modules.AbstractModule;
+import org.jfree.base.modules.ModuleInitializeException;
+import org.jfree.base.modules.SubSystem;
+import org.jfree.report.ext.modules.java14print.Java14PrintModuleInitializer;
+
+/**
+ * The module definition for the Java 1.4 printing support module.
+ *
+ * @author Thomas Morgner
+ */
+public class Java14PrintModule extends AbstractModule
+{
+  /**
+   * DefaultConstructor. Loads the module specification.
+   * @throws org.jfree.base.modules.ModuleInitializeException if an error occured.
+   */
+  public Java14PrintModule() throws ModuleInitializeException
+  {
+    loadModuleInfo();
+  }
+
+  /**
+   * Initializes the module. Use this method to perform all initial setup operations. This
+   * method is called only once in a modules lifetime. If the initializing cannot be
+   * completed, throw a ModuleInitializeException to indicate the error,. The module will
+   * not be available to the system.
+   *
+   * @param subSystem the subSystem.
+   * @throws org.jfree.base.modules.ModuleInitializeException
+   *          if an error ocurred while initializing the module.
+   */
+  public void initialize (SubSystem subSystem)
+          throws ModuleInitializeException
+  {
+    performExternalInitialize(Java14PrintModuleInitializer.class.getName());
+  }
+
+}
---
+++ jfreereport-ext/source/org/jfree/report/ext/modules/output/java14print/Java14PrintModuleInitializer.java
@@ -0,0 +1,57 @@
+/**
+ * ===========================================
+ * JFreeReport : a free Java reporting library
+ * ===========================================
+ *
+ * Project Info:  http://www.jfree.org/liblayout/
+ *
+ * (C) Copyright 2006, by Pentaho Corperation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ * ------------
+ * Java14PrintModuleInitializer.java
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corperation.
+ */
+
+package org.jfree.report.ext.modules.output.java14print;
+
+import org.jfree.base.modules.ModuleInitializer;
+import org.jfree.base.modules.ModuleInitializeException;
+
+/**
+ * An initializer for the Java 1.4 Image-I/O API.
+ * @author Thomas Morgner
+ */
+public class Java14PrintModuleInitializer implements ModuleInitializer
+{
+  /**
+   * Default Constructor.
+   */
+  public Java14PrintModuleInitializer()
+  {
+  }
+
+  /**
+   * Initializes the module and defines the storage implementation.
+   *
+   * @throws org.jfree.base.modules.ModuleInitializeException if an error ocurred.
+   */
+  public void performInit() throws ModuleInitializeException
+  {
+  }
+}
