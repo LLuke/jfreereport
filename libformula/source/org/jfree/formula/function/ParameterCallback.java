313dbc75989b0fd5df3349de3158d0f04e12968d
taqua <taqua>
2006-11-13 19:15:49 +0000

    callbacks ..

---
+++ libformula/source/org/jfree/formula/function/ParameterCallback.java
@@ -0,0 +1,47 @@
+/**
+ * =========================================
+ * LibFormula : a free Java formula library
+ * =========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/libformula
+ *
+ * (C) Copyright 2006, by Pentaho Corperation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ *
+ * ------------
+ * $Id$
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corperation.
+ */
+
+package org.jfree.formula.function;
+
+import org.jfree.formula.typing.Type;
+import org.jfree.formula.EvaluationException;
+
+/**
+ * Creation-Date: 13.11.2006, 13:52:21
+ *
+ * @author Thomas Morgner
+ */
+public interface ParameterCallback
+{
+  public Object getValue (int position) throws EvaluationException;
+  public Type getType (int position) throws EvaluationException;
+  public int getParameterCount();
+}
