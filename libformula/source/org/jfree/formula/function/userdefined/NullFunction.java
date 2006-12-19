264fadb95e5233177dbbd9e5f20b60c746502a8e
taqua <taqua>
2006-12-19 18:05:10 +0000

    NullFunction added

---
+++ libformula/source/org/jfree/formula/function/userdefined/NullFunction.java
@@ -0,0 +1,61 @@
+/**
+ * =========================================
+ * LibFormula : a free Java formula library
+ * =========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/libformula
+ *
+ * (C) Copyright 2006, by Pentaho Corporation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ *
+ * ------------
+ * $Id$
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ */
+
+package org.jfree.formula.function.userdefined;
+
+import org.jfree.formula.FormulaContext;
+import org.jfree.formula.function.ParameterCallback;
+import org.jfree.formula.lvalues.TypeValuePair;
+import org.jfree.formula.typing.coretypes.AnyType;
+
+/**
+ * Creation-Date: Dec 18, 2006, 12:54:55 PM
+ *
+ * @author Thomas Morgner
+ */
+public class NullFunction
+{
+  public NullFunction()
+  {
+  }
+
+  public String getCanonicalName()
+  {
+    return "NULL";
+  }
+
+  public TypeValuePair evaluate(FormulaContext context,
+                                ParameterCallback parameters)
+  {
+    return new TypeValuePair(AnyType.TYPE, null);
+  }
+
+}
---
+++ libformula/source/org/jfree/formula/function/userdefined/NullFunctionDescription.java
@@ -0,0 +1,101 @@
+/**
+ * =========================================
+ * LibFormula : a free Java formula library
+ * =========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/libformula
+ *
+ * (C) Copyright 2006, by Pentaho Corporation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ *
+ * ------------
+ * $Id$
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ */
+
+package org.jfree.formula.function.userdefined;
+
+import org.jfree.formula.function.AbstractFunctionDescription;
+import org.jfree.formula.function.FunctionCategory;
+import org.jfree.formula.typing.Type;
+import org.jfree.formula.typing.coretypes.AnyType;
+
+/**
+ * Creation-Date: 04.11.2006, 18:28:55
+ *
+ * @author Thomas Morgner
+ */
+public class NullFunctionDescription extends AbstractFunctionDescription
+{
+  public NullFunctionDescription()
+  {
+    super("org.jfree.formula.function.userdefined.Null-Function");
+  }
+
+  public int getParameterCount()
+  {
+    return 0;
+  }
+
+  public boolean isInfiniteParameterCount()
+  {
+    return false;
+  }
+
+  public Type getParameterType(int position)
+  {
+    return AnyType.TYPE;
+  }
+
+  public Type getValueType()
+  {
+    return AnyType.TYPE;
+  }
+
+  /**
+   * Defines, whether the parameter at the given position is mandatory. A
+   * mandatory parameter must be filled in, while optional parameters need not
+   * to be filled in.
+   *
+   * @return
+   */
+  public boolean isParameterMandatory(int position)
+  {
+    return false;
+  }
+
+  /**
+   * Returns the default value for an optional parameter. If the value returned
+   * here is null, then this either means, that the parameter is mandatory or
+   * that the default value is computed by the expression itself.
+   *
+   * @param position
+   * @return
+   */
+  public Object getDefaultValue(int position)
+  {
+    return null;
+  }
+
+  public FunctionCategory getCategory()
+  {
+    return UserDefinedFunctionCategory.CATEGORY;
+  }
+
+}
