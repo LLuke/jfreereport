3ce722d4199205c1edeecd5338df5a6c1999522c
taqua <taqua>
2006-12-22 10:22:30 +0000

    New files

---
+++ libformula/source/org/jfree/formula/function/information/IsBlankFunction.java
@@ -0,0 +1,74 @@
+/**
+ * =========================================
+ * LibFormula : a free Java formula library
+ * =========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/libformula
+ *
+ * (C) Copyright 2006, by Pentaho Corporation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ *
+ * ------------
+ * $Id$
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ */
+
+package org.jfree.formula.function.information;
+
+import org.jfree.formula.EvaluationException;
+import org.jfree.formula.FormulaContext;
+import org.jfree.formula.LibFormulaErrorValue;
+import org.jfree.formula.function.Function;
+import org.jfree.formula.function.ParameterCallback;
+import org.jfree.formula.lvalues.TypeValuePair;
+import org.jfree.formula.typing.coretypes.ErrorType;
+import org.jfree.formula.typing.coretypes.LogicalType;
+
+/**
+ * Document me!
+ *
+ * @author : Thomas Morgner
+ */
+public class IsBlankFunction implements Function
+{
+  public String getCanonicalName()
+  {
+    return "ISBLANK";
+  }
+
+  public TypeValuePair evaluate(FormulaContext context, ParameterCallback parameters) throws EvaluationException
+  {
+    final int parameterCount = parameters.getParameterCount();
+    if (parameterCount < 1)
+    {
+      return new TypeValuePair(ErrorType.TYPE, new LibFormulaErrorValue(1));
+    }
+
+    if (parameters.getValue(0) == null)
+    {
+      return new TypeValuePair(LogicalType.TYPE, Boolean.TRUE);
+    }
+
+    return new TypeValuePair(LogicalType.TYPE, Boolean.FALSE);
+  }
+
+  public IsBlankFunction()
+  {
+  }
+}
