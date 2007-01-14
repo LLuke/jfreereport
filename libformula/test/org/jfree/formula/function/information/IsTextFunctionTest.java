dccaaf28f9bc5b2abe21f478f225b985088f8622
mimil <mimil>
2007-01-14 18:28:57 +0000

    Add of headers

--- libformula/test/org/jfree/formula/function/information/IsTextFunctionTest.java
+++ libformula/test/org/jfree/formula/function/information/IsTextFunctionTest.java
@@ -1,10 +1,39 @@
+/**
+ * =========================================
+ * LibFormula : a free Java formula library
+ * =========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/libformula
+ *
+ * (C) Copyright 2006, by Pentaho Corporation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ *
+ * ------------
+ * $Id: DateFunction.java,v 1.6 2006/12/30 14:54:38 taqua Exp $
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ */
 package org.jfree.formula.function.information;
 
 import org.jfree.formula.EvaluationException;
 import org.jfree.formula.Formula;
 import org.jfree.formula.FormulaContext;
 import org.jfree.formula.LibFormulaBoot;
-import org.jfree.formula.LibFormulaErrorValue;
 import org.jfree.formula.common.TestFormulaContext;
 import org.jfree.formula.parser.ParseException;
 import org.testng.Assert;
@@ -12,6 +41,11 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+/**
+ * 
+ * @author Cedric Pronzato
+ *
+ */
 public class IsTextFunctionTest
 {
   private FormulaContext context;
--- libformula/test/org/jfree/formula/function/text/LenFunctionTest.java
+++ libformula/test/org/jfree/formula/function/text/LenFunctionTest.java
@@ -1,3 +1,33 @@
+/**
+ * =========================================
+ * LibFormula : a free Java formula library
+ * =========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/libformula
+ *
+ * (C) Copyright 2006, by Pentaho Corporation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ *
+ * ------------
+ * $Id: DateFunction.java,v 1.6 2006/12/30 14:54:38 taqua Exp $
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ */
 package org.jfree.formula.function.text;
 
 import java.math.BigDecimal;
@@ -13,6 +43,11 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+/**
+ * 
+ * @author Cedric Pronzato
+ *
+ */
 public class LenFunctionTest
 {
   private FormulaContext context;
