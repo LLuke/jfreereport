5876edc4179f6e08ea2497b65f6f7a46f1b4d14e
taqua <taqua>
2007-02-21 19:22:31 +0000

    Removed a lot of bad code - everything !

--- jfreereport-tools/source/org/jfree/report/dev/beans/templates/Template.java
+++
@@ -1,8 +0,0 @@
-package org.jfree.report.dev.beans.templates;
-
-import java.io.PrintWriter;
-
-public interface Template
-{
-  public void print (PrintWriter writer, Context context) throws TemplateException;
-}
