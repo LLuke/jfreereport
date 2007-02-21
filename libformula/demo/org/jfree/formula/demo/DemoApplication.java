e270265ddc5a891b49420e9d4409542cda389749
taqua <taqua>
2007-02-21 14:31:25 +0000

    Simple Demo application added

---
+++ libformula/demo/org/jfree/formula/demo/DemoApplication.java
@@ -0,0 +1,69 @@
+/**
+ * =========================================
+ * LibFormula : a free Java formula library
+ * =========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/libformula
+ *
+ * (C) Copyright 2006, by Pentaho Corporation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ *
+ * ------------
+ * $Id$
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ */
+
+package org.jfree.formula.demo;
+
+import javax.swing.JOptionPane;
+
+import org.jfree.formula.LibFormulaBoot;
+import org.jfree.formula.Formula;
+import org.jfree.formula.EvaluationException;
+import org.jfree.formula.parser.ParseException;
+
+/**
+ * Creation-Date: Feb 21, 2007, 2:55:48 PM
+ *
+ * @author Thomas Morgner
+ */
+public class DemoApplication
+{
+  public static void main(String[] args)
+      throws ParseException, EvaluationException
+  {
+    LibFormulaBoot.getInstance().start();
+
+    final String formula = JOptionPane.showInputDialog
+        ("Please enter a formula.");
+
+    // first parse the formula. This checks the general syntax, but does not
+    // check whether the used functions or references are actually valid.
+    final Formula f = new Formula(formula);
+
+    // connects the parsed formula to the context. The context provides the
+    // operator and function implementations and resolves the references.
+    f.initialize(new DemoFormulaContext());
+
+    JOptionPane.showMessageDialog(null, "The result is " + f.evaluate(),
+        "Result", JOptionPane.INFORMATION_MESSAGE);
+
+    System.exit(0);
+  }
+}
---
+++ libformula/demo/org/jfree/formula/demo/DemoFormulaContext.java
@@ -0,0 +1,68 @@
+/**
+ * =========================================
+ * LibFormula : a free Java formula library
+ * =========================================
+ *
+ * Project Info:  http://jfreereport.pentaho.org/libformula
+ *
+ * (C) Copyright 2006, by Pentaho Corporation and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ *
+ *
+ * ------------
+ * $Id$
+ * ------------
+ * (C) Copyright 2006, by Pentaho Corporation.
+ */
+
+package org.jfree.formula.demo;
+
+import javax.swing.JOptionPane;
+
+import org.jfree.formula.DefaultFormulaContext;
+import org.jfree.formula.typing.Type;
+import org.jfree.formula.typing.coretypes.AnyType;
+
+/**
+ * A formula context that asks the user for input.
+ *
+ * @author Thomas Morgner
+ */
+public class DemoFormulaContext extends DefaultFormulaContext
+{
+  public DemoFormulaContext()
+  {
+  }
+
+  public Object resolveReference(Object name)
+  {
+    final String input = JOptionPane.showInputDialog
+        ("Please enter a value for '" + name + "'");
+    return input;
+  }
+
+  public Type resolveReferenceType(Object name)
+  {
+    // by returning the correct type of the reference, you can speed up
+    // the formula computation a little bit, as we dont have to guess the
+    // type from scratch.
+
+    // If you dont know the type, return ANYTYPE. We will start looking at
+    // the referenced object in that case.
+    return AnyType.TYPE;
+  }
+}
